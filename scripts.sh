d669d57adb58e693c5be102eb8d07035d0b89836
MIT-X <134132915+MIT-X@users.noreply.github.com>
2026-06-21 16:11:18 +0800

    Add files via upload

---
+++ scripts.sh
@@ -0,0 +1,12 @@
+# 此脚本用处是：添加第三方插件
+#=========================================================================================================================
+
+# 1-添加 集客 插件
+git clone https://github.com/laipeng668/luci-app-gecoosac.git package/luci-app-gecoosac
+
+# 2-添加 PassWall2 插件
+echo "src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+
+# 3-添加 DAED 插件
+# git clone https://github.com/QiuSimons/luci-app-daed.git package/dae
